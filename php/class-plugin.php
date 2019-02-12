e9fe35fe4bfc0041cbebcaf08599fca8aa51ee22
Ryan Kienstra <kienstraryan@gmail.com>
2019-02-11 20:51:27 -0600

    Add *-compiled.js to the .gitignore file
    Prevent this from appearing in GitHub Desktop,
    or whatever Git tool is used.

--- php/class-plugin.php
+++ php/class-plugin.php
@@ -62,7 +62,6 @@ class Plugin {
 		$this->init_classes();
 		$this->plugin_url = plugins_url( self::SLUG );
 		add_action( 'init', array( $this, 'plugin_localization' ) );
-		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 	}
 
 	/**
