808fd009af3e741bc8866fd6baf322615195f22c
Ryan Kienstra <kienstraryan@gmail.com>
2019-02-19 01:00:58 -0600

    In .eslintrc, copy "parserOptions" from AMP plugin
    Copy from Official AMP Plugin for WordPress,
    in order to address an eslint error.
    Also, correct eslint errors in block index.js file.

--- blocks/ar-viewer/index.js
+++ blocks/ar-viewer/index.js
@@ -1,15 +1,14 @@
 /**
- * General block libraries.
+ * General block libraries and the block name.
  */
-const { __ } = wp.i18n;
-const { registerBlockType } = wp.blocks;
-const { MediaUpload } = wp.editor;
-const {
-	Button,
-	Placeholder
-} = wp.components;
-
-const arViewerBlock = 'augmented-reality/ar-viewer';
+const { __ } = wp.i18n,
+	{ registerBlockType } = wp.blocks,
+	{ MediaUpload } = wp.editor,
+	{
+		Button,
+		Placeholder
+	} = wp.components,
+	arViewerBlock = 'augmented-reality/ar-viewer';
 
 /**
  * Registers the AR Viewer block.
@@ -33,52 +32,57 @@ export default registerBlockType(
 			},
 		},
 
+		/**
+		 * Gets the block editor UI.
+		 *
+		 * @param {Object} props The editor properties.
+		 * @returns {string} The markup of the editor UI.
+		 */
 		edit: props => {
 			const { attributes: { objUrl, mtlUrl },
-				className, noticeUI, setAttributes } = props;
-
-			const getViewerWrapper = ( url, fileType ) => {
-				return (
-					<div className="ar-viewer-wrapper">
-						{ ! url ? (
-							<MediaUpload
-								onSelect={ img => {
-									let attributes = {};
-									attributes[ fileType + 'Url' ] = img.url;
-									setAttributes( attributes );
-								} }
-								type="image"
-								value={ url }
-								render={ ( { open } ) => (
-									<Button
-										className={ 'button button-large' }
-										onClick={ open }
-									>
-										{ __( 'Select %s file', 'augmented-reality' ).replace( '%s', fileType ) }
-									</Button>
-								) }
-							>
-							</MediaUpload>
-						) : (
-							<div>
-								<span>{ url.match( '[^\\/]+\\.+' + fileType + '$' ) ? url.match( '[^\\/]+\\.+' + fileType + '$' )[0] : null }</span>
-								{ !! url ? (
-									<Button
-										className="button button-large"
-										onClick={ () => {
-											let attributes = {};
-											attributes[ fileType + 'Url' ] = null;
-											setAttributes( attributes );
-										} }
-									>
-										{ __( 'Remove %s file', 'augmented-reality' ).replace( '%s', fileType ) }
-									</Button>
-								) : null }
-							</div>
-						) }
-					</div>
-				)
-			};
+					className, noticeUI, setAttributes } = props,
+				getViewerWrapper = ( url, fileType ) => {
+					return (
+						<div className="ar-viewer-wrapper">
+							{ ! url ?
+								<MediaUpload
+									onSelect={ img => {
+										let attributes = {};
+										attributes[ fileType + 'Url' ] = img.url;
+										setAttributes( attributes );
+									} }
+									type="image"
+									value={ url }
+									render={ ( { open } ) =>
+										<Button
+											className={ 'button button-large' }
+											onClick={ open }
+										>
+											{ __( 'Select %s file', 'augmented-reality' ).replace( '%s', fileType ) }
+										</Button>
+									}
+								>
+								</MediaUpload>
+								:
+								<div>
+									<span>{ url.match( '[^\\/]+\\.+' + fileType + '$' ) ? url.match( '[^\\/]+\\.+' + fileType + '$' )[0] : null }</span>
+									{ url ?
+										<Button
+											className="button button-large"
+											onClick={ () => {
+												let attributes = {};
+												attributes[ fileType + 'Url' ] = null;
+												setAttributes( attributes );
+											} }
+										>
+											{ __( 'Remove %s file', 'augmented-reality' ).replace( '%s', fileType ) }
+										</Button>
+										: null }
+								</div>
+							}
+						</div>
+					)
+				};
 
 			return (
 				<div className={ className }>
@@ -101,9 +105,10 @@ export default registerBlockType(
 		 * Renders in PHP.
 		 *
 		 * @see Block::render_block().
+		 * @returns {null} Rendered in PHP.
 		 */
 		save: () => {
 			return null;
 		},
-	},
+	}
 );
