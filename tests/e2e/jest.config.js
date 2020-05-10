b5830877bed9150fda88cd65ce1f33d64a6b99c5
Ryan Kienstra <kienstraryan@gmail.com>
2020-05-10 15:57:42 -0500

    Add axe accessibility tests back in setup
    Also, fix an error in .travis.yml where
    the builds didn't run because matrix wasn't
    indented, I think.

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	setupFilesAfterEnv: [
 		'<rootDir>/config/setup-test-framework.js',
 		'@wordpress/jest-console',
+		'@wordpress/jest-puppeteer-axe',
 		'expect-puppeteer',
 	],
 	testPathIgnorePatterns: [
